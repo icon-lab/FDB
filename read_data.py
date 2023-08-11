bfe58d27b30806c24746b11acab44c9bae0a2ef8
usamamirza0 <76936167+usamamirza0@users.noreply.github.com>
2023-08-11 12:21:23 +0300

    Add files via upload

---
+++ read_data.py
@@ -0,0 +1,129 @@
+import numpy as np
+import h5py
+
+def LoadDataSetSingleCoil(load_dir, variable = "data_fs"):
+    f = h5py.File(load_dir, 'r')
+    if variable == "data_fs" or variable == "us_masks":
+        data = np.expand_dims(np.transpose(np.array(f[variable]), (0,2,1)), axis=1).astype(np.float32)
+    elif variable == "data_us":
+        data = np.transpose(np.array(f[variable]), (1,0,3,2))
+        phase_ = (data[:,1,:,:] * 2 * np.pi) - np.pi
+        data = data[:,0,:,:] * np.exp(1j * phase_)
+    return data
+
+def LoadDataSetMultiCoil(load_dir, variable = 'images_fs', padding = True, Norm = True, res = [384, 384], slices = 10, is_complex = True, channel_cat = False):
+    f = h5py.File(load_dir,'r')
+
+    if np.array(f[variable]).ndim==3:
+        data=np.expand_dims(np.transpose(np.array(f[variable]),(0,1,2)),axis=1)
+    else:
+        data=np.transpose(np.array(f[variable]),(1,0,2,3))
+
+    if is_complex:
+        data  = data['real'] + 1j*data['imag']    
+    else:
+        data=data.astype(np.float32)
+
+    if Norm:
+        #normalize each subject    
+        subjects=int(data.shape[0]/slices )
+        data=np.split(data,subjects,axis=0)  
+        data=[x/abs(x).max() for x in data]
+        data=np.concatenate(data,axis=0)  
+    
+    if channel_cat:    
+        data  = np.concatenate((data.real, data.imag), axis=1)
+
+    if padding:
+        pad_x = int((res[0]-data.shape[2])/2); pad_y = int((res[1]-data.shape[3])/2)
+        data=np.pad(data,((0,0),(0,0),(pad_x, pad_x),(pad_y, pad_y)))  
+
+    return data
+
+def get_fs_singlecoil(data_dir, phase = 'train'):
+    """
+    Returns fully sampled singlecoilimages of shape
+    [Number of images x height x width]
+    """
+
+    print("Loading T1 images")
+    target_file = data_dir + "T1_2_multi_synth_recon_" + str(phase) + ".mat"
+    data_fs_t1 = LoadDataSetSingleCoil(target_file)
+
+    print("Loading T2 images")
+    target_file = data_dir + "T2_2_multi_synth_recon_" + str(phase) + ".mat"
+    data_fs_t2 = LoadDataSetSingleCoil(target_file)
+
+    print("Loading PD images")
+    target_file = data_dir + "PD_2_multi_synth_recon_" + str(phase) + ".mat"
+    data_fs_pd = LoadDataSetSingleCoil(target_file)
+
+    data_fs = np.concatenate((data_fs_t1, data_fs_t2, data_fs_pd), axis = 0)
+    data_fs = np.squeeze(data_fs)
+    print("data_fs.shape: " + str(data_fs.shape))
+    return data_fs
+
+
+def get_us_singlecoil(data_dir, phase = 'test', contrast= 'T1', R = 4):
+    """
+    Returns singlecoil undersampled images and undersampling masks of shape
+    [Number of images x height x width]
+    """
+
+    print("Loading " + contrast + " " + str(R) + "x Data")
+    target_file = data_dir + contrast + "_" + str(R) + "_multi_synth_recon_" + str(phase) + ".mat"
+    data_us = LoadDataSetSingleCoil(target_file, variable = "data_us")
+    us_masks = LoadDataSetSingleCoil(target_file, variable = "us_masks")
+
+    data_us = np.squeeze(data_us)
+    us_masks = np.squeeze(us_masks)
+
+    print(data_us.shape, us_masks.shape)
+
+    return data_us, us_masks
+
+def get_fs_multicoil(data_dir, phase = 'train'):
+    """ Returns fully sampled multicoil images of shape
+    [Number of images x height x width]
+    """
+
+    print("Loading T1 images")
+    target_file = data_dir + "T1/T1_under_sampled_2x_multicoil_" + str(phase)+ ".mat"
+    data_fs_t1 = LoadDataSetMultiCoil(target_file)
+
+    print("Loading T2 images")
+    target_file = data_dir + "T2/T2_under_sampled_2x_multicoil_" + str(phase) + ".mat"
+    data_fs_t2 = LoadDataSetMultiCoil(target_file)
+
+    print("Loading FLAIR images")
+    target_file = data_dir + "FLAIR/FLAIR_under_sampled_2x_multicoil_" + str(phase)+ ".mat"
+    data_fs_flair=LoadDataSetMultiCoil(target_file)
+   
+    data_fs=np.concatenate((data_fs_t1,data_fs_t2,data_fs_flair),axis=0)
+    data_fs = np.squeeze(data_fs)
+
+    print(data_fs.shape)
+
+    return data_fs
+
+def get_us_multicoil(data_dir, phase='test', contrast= 'T1', R = 4):
+    """
+    Returns undersampled images of shape
+    [Number of images x number of coils x height x width],
+    undersampling masks of shape
+    [Number of images x 1 x height x width],
+    coil sensitivity maps of shape
+    [Number of images x number of coils x height x width]
+    """
+
+    print("Loading " + contrast + " " + str(R) + "x Data")
+
+    target_file = data_dir + contrast + "/" + contrast + "_under_sampled_" + str(R) + "x_multicoil_" + str(phase) + ".mat"
+    data_fs = LoadDataSetMultiCoil(target_file, 'images_fs', padding = False, Norm = True, channel_cat = False)
+    data_us = LoadDataSetMultiCoil(target_file, 'images_us', padding = False, Norm = True, channel_cat = False)        
+    masks = LoadDataSetMultiCoil(target_file, 'map', padding = False, Norm = False, is_complex = False, channel_cat = False)                    
+    coil_maps = LoadDataSetMultiCoil(target_file, 'coil_maps', padding = False, Norm = False, channel_cat = False)
+
+    print(data_us.shape, masks.shape, coil_maps.shape)
+
+    return data_us, masks, coil_maps
---
+++ sample.py
@@ -0,0 +1,108 @@
+import argparse
+import os
+import time
+import numpy as np
+import torch as th
+import torch.distributed as dist
+import read_data
+from utils import dist_util, logger
+from utils.script_util_duo import (
+    model_and_diffusion_defaults,
+    create_model_and_diffusion,
+    add_dict_to_argparser,
+    args_to_dict,
+)
+import imageio
+
+
+def main():
+    args = create_argparser().parse_args()
+
+    if args.data_type == "singlecoil":
+        images, masks = read_data.get_us_singlecoil(args.data_path, R=args.R, contrast=args.contrast)
+    elif args.data_type == "multicoil":
+        images, masks, coil_maps = read_data.get_us_multicoil(args.data_path, R=args.R, contrast=args.contrast)
+
+    dist_util.setup_dist()
+    logger.configure(dir=args.save_path)
+
+    logger.log("creating model and diffusion...")
+    model, diffusion = create_model_and_diffusion(
+        **args_to_dict(args, model_and_diffusion_defaults().keys())
+    )
+    model.load_state_dict(
+        dist_util.load_state_dict(args.model_path, map_location="cpu")
+    )
+    model.to(dist_util.dev())
+
+    model.eval()
+    for index in range(len(images)):
+        mask = th.from_numpy(masks[np.newaxis,np.newaxis,index]).cuda()
+        mask = th.cat([mask,mask],1)
+
+        kspace = load_data(images, index, args.batch_size, args.data_type)
+
+        if args.data_type == "multicoil":
+            coil_map = th.from_numpy(coil_maps[np.newaxis, index, :, :, :]).cuda()
+        else:
+            coil_map = None
+
+        start_time = time.time()
+        print("Index:", index)
+        sample = diffusion.p_sample_loop_condition(
+            model,
+            (args.batch_size, 2, args.image_size, args.image_size),
+            kspace,
+            mask,
+            coil_map
+        )[-1]
+        total_time = time.time() - start_time
+        print("total time: " + str(total_time))
+        samples = []
+        coarse = []
+        samples.append(sample)
+        optimizerG = th.optim.Adam(model.parameters(), lr=1e-4)
+
+        samples = th.cat(samples)
+        coarse.append(samples.contiguous())
+        coarse = th.stack(coarse)
+        coarse_np = coarse.cpu().data.numpy()
+        np.save(os.path.join(args.save_path, "coarse" + str(index)  + ".npy"), coarse_np)
+
+        vis_1 = np.abs(coarse_np[0,-1,0,:,:] + coarse_np[0,-1,1,:,:]*1j)
+        imageio.imsave(os.path.join(args.save_path, "image" + '_' + str(index) + '.png'), vis_1/vis_1.max())
+
+
+def load_data(dataset, index, batch_size, data_type):
+
+    img_prior = dataset[index]
+
+    kspace1 = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(img_prior, axes=[-1,-2])), axes=[-1,-2])
+
+    if data_type == "singlecoil":
+        kspace = th.FloatTensor(np.stack([np.real(kspace1), np.imag(kspace1)])).cuda().view(1, 2, img_prior.shape[-2], img_prior.shape[-1]).repeat(batch_size, 1, 1, 1).float()
+    elif data_type == "multicoil":
+        kspace = th.from_numpy(kspace1).cuda().view(1, 5, img_prior.shape[-2], img_prior.shape[-1]).repeat(batch_size, 1, 1, 1)
+    return kspace
+
+def create_argparser():
+    defaults = dict(
+        num_samples=100,
+        batch_size=5,
+        use_ddim=False,
+        model_path="",
+        data_path="",
+        save_path="",
+        beta1 = 0.9,
+        beta2 = 0.99,
+        R = 4,
+        contrast = "T1",
+    )
+    defaults.update(model_and_diffusion_defaults())
+    parser = argparse.ArgumentParser()
+    add_dict_to_argparser(parser, defaults)
+    return parser
+
+
+if __name__ == "__main__":
+    main()
